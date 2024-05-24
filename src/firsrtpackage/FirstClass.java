30a8e1a3acf201da7e7b218558e30a9e7ef0e9f3
Harrison Martinez <jonathan.martinez@corusconsulting.com>
2024-05-23 19:49:38 -0500

    Third Modify

--- src/firsrtpackage/FirstClass.java
+++ src/firsrtpackage/FirstClass.java
@@ -3,5 +3,6 @@ package firsrtpackage;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First line");
+        System.out.println("Second line");
     }
 }
